82de16c4a7bd429d8fc052305bb65e3ff5a99fa2
PangXieOffice <hsw625728@163.com>
2017-01-23 17:40:46 +0800

    立项上传

---
+++ mhsyCoredata/mhsyCoredata/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  mhsyCoredata
+//
+//  Created by HANSHAOWEN on 17/1/23.
+//  Copyright © 2017年 HANSHAOWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ mhsyCoredata/mhsyCoredata/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  mhsyCoredata
+//
+//  Created by HANSHAOWEN on 17/1/23.
+//  Copyright © 2017年 HANSHAOWEN. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
